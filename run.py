a88ba5c8a8c60ae85346e99a3aa735c2bb25b570
zetian <zetian_w@163.com>
2024-11-28 11:50:25 +0800

    test_allure

--- run.py
+++ run.py
@@ -1,13 +1,19 @@
 # 开发人员：泽天
+# import os
+#
+# import pytest
+#
+# # 开发时间：2024/4/1 16:24
+#
+# # 项目定义：
+# if __name__ == '__main__':
+#     pytest.main()
+#     os.system("allure generate ./reports -o ./reports/temp --clean")
+#  #   os.system("allure generate ./reports/assets -o ./reports/temp --clean")
 import os
-
 import pytest
 
-# 开发时间：2024/4/1 16:24
-
-# 项目定义：
 if __name__ == '__main__':
-    pytest.main()
-#    os.system("allure generate ./reports -o ./reports/temp --clean")
- #   os.system("allure generate ./reports/assets -o ./reports/temp --clean")
+    pytest.main(["-vs", "--alluredir=./reports/temp"])
+    os.system("allure generate ./reports/temp -o ./reports/html --clean")
 
